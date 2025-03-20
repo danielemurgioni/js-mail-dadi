01a598fefd6a419f332e8e206356f91c8fa6d897
danielemurgioni <dashomew@gmail.com>
2025-03-20 14:59:17 +0100

    [FEAT] Created cycle for to check invitedEmails

--- js/script.js
+++ js/script.js
@@ -1,7 +1,11 @@
 const invitedEmails = ["pippio@email.com", "franco@email.com", "mario@email.com", "checco@email.com"];
 
-console.log(invitedEmails)
+console.log(invitedEmails);
 
-let userEmail = prompt("Scrivi qui la tua email")
+let userEmail = prompt("Scrivi qui la tua email");
 
-console.log(userEmail)
+console.log(userEmail);
+
+for(let i=0; i<invitedEmails.length; i++){
+    
+}
